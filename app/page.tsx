da583b18e8760dcfe74077256b6280ee28b393dd
AbdulSamad <abdulsamadpieces@gmail.com>
2024-12-12 14:22:29 +0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', margin: '50px' }}>
-    <h1>Welcome to My Static Demo Sitedfdlfdfldfld</h1>
+    <h1>Welcome to My Static Demo </h1>
     <p>This is a simple static page built with Next.js!</p>
     <img src="/demo-image.jpg" alt="Demo Image" style={{ maxWidth: '100%', height: 'auto' }} />
   </div>
